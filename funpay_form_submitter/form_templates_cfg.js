88783edf1fdbcccfa448183006d701be27eff44e
Artem <akovalev@storm-pro.net>
2025-01-09 03:09:41 +0300

    AI BEST

--- funpay_form_submitter/form_templates_cfg.js
+++ funpay_form_submitter/form_templates_cfg.js
@@ -3,6 +3,13 @@ import { FunPayFormProcessor } from './form_data_processor.js';
 import templates from './form_templates.json' assert { type: 'json' };
 import { calculatePrice } from './price_calculator.js';
 
+// Функция для извлечения значений полей из опций
+function extractFieldValue(template, fieldName, offer) {
+    if (!template[fieldName]) return ' ';
+    return Object.keys(template[fieldName])
+        .find(value => offer.options?.includes(value)) || ' ';
+}
+
 // Функция для генерации шаблона из JSON
 function generateTemplateFromJson(nodeId) {
     try {
@@ -32,14 +39,13 @@ function generateTemplateFromJson(nodeId) {
                             'fields[desc][ru]': config.descRu,
                             'fields[payment_msg][ru]': ' ',
                             price: calculatePrice("965", offer.price),
-                            deactivate_after_sale: '',
                             amount: '999'
                         };
 
                     case "1142": // Arknights
                         return {
                             ...formData,
-                            'fields[type]': 'Паки',
+                            'fields[type]': extractFieldValue(template, 'fields[type]', offer),
                             'fields[summary][ru]': FunPayFormProcessor.formatDescription(offer.descText),
                             'fields[summary][en]': FunPayFormProcessor.formatDescription(offer.descTextEn),
                             'fields[desc][ru]': config.descRu,
@@ -54,7 +60,7 @@ function generateTemplateFromJson(nodeId) {
                         return {
                             ...formData,
                             server_id: '9074',
-                            'fields[method]': 'С заходом на аккаунт',
+                            'fields[method]': extractFieldValue(template, 'fields[method]', offer),
                             'fields[summary][ru]': FunPayFormProcessor.formatDescription(offer.descText),
                             'fields[summary][en]': '',
                             'fields[desc][ru]': config.descRu,
@@ -62,30 +68,16 @@ function generateTemplateFromJson(nodeId) {
                             'fields[payment_msg][ru]': ' ',
                             'fields[payment_msg][en]': ' ',
                             'fields[images]': '',
-                            price: calculatePrice("1560", offer.price),
-                            deactivate_after_sale: ''
+                            price: calculatePrice("1560", offer.price)
                         };
 
                     case "1127": // Brawl Stars
-                        // Находим значения из доступных опций
-                        const typeValue = Object.keys(template['fields[type]'])
-                            .find(type => offer.options?.includes(type));
-                            
-                        const methodValue = Object.keys(template['fields[method]'])
-                            .find(method => offer.options?.includes(method));
-                            
-                        const type2Value = Object.keys(template['fields[type2]'])
-                            .find(type2 => offer.options?.includes(type2));
-                            
-                        const brawlpassValue = Object.keys(template['fields[brawlpass]'])
-                            .find(brawlpass => offer.options?.includes(brawlpass)) || ' ';
-                            
                         return {
                             ...formData,
-                            'fields[method]': methodValue,
-                            'fields[type]': typeValue,
-                            'fields[type2]': type2Value,
-                            'fields[brawlpass]': brawlpassValue,
+                            'fields[method]': extractFieldValue(template, 'fields[method]', offer),
+                            'fields[type]': extractFieldValue(template, 'fields[type]', offer),
+                            'fields[type2]': extractFieldValue(template, 'fields[type2]', offer),
+                            'fields[brawlpass]': extractFieldValue(template, 'fields[brawlpass]', offer),
                             'fields[summary][ru]': FunPayFormProcessor.formatDescription(offer.descText),
                             'fields[desc][ru]': config.descRu,
                             price: calculatePrice("1127", offer.price)
